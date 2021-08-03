76720252b540c2e7436da0b6b813334140be59cb
Igor Blinnikov <igor.blinnikov@gmail.com>
2021-08-03 22:04:28 +0300

    feat(memorize): initial project structure

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Igor Blinnikov on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
