2d7947d43400f3460c329b21e16763724fb2c3b8
Igor Blinnikov <igor.blinnikov@gmail.com>
2021-08-28 17:21:30 +0300

    chore(cards): - introducing CardView - add preview for both dark/light modes

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, world!")
-                
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(.red)
@@ -22,8 +22,26 @@ struct ContentView: View {
     }
 }
 
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("✈️")
+                .font(.largeTitle)
+                
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
